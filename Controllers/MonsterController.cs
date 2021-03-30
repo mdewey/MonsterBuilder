b55e35b2ddff89f6e855a78846aa998cff846cff
Mark Dewey <mark.t.dewey@gmail.com>
2021-03-29 21:37:07 -0400

    added type and subtype

--- Controllers/MonsterController.cs
+++ Controllers/MonsterController.cs
@@ -41,13 +41,33 @@ namespace MonsterBuilder.Controllers
           return !String.IsNullOrEmpty(w.InnerHtml.Trim());
         }).Select(s => s).ToList();
 
-
+      var i = 0;
       foreach (var item in data)
       {
-        Console.WriteLine($"{item.InnerText}");
+        Console.WriteLine($"{i}=>{item.InnerText}");
         Console.WriteLine("-------");
+        i++;
       }
       rv.Name = data[0].InnerText;
+      rv.ChallengeRating = data[2].InnerText.Split(" ").Last();
+      // N Huge magical beast (aquatic, augmented animal)
+      var details = data[7].InnerText;
+      var splat = details.Split(' ');
+      rv.Alignment = splat[0];
+      rv.Size = splat[1];
+      rv.Type = String.Join(String.Empty, details.Skip(rv.Alignment.Length + rv.Size.Length + 2)).Split($"(")[0];
+      if (details.Contains($"("))
+      {
+        rv.SubType = details.Substring(details.IndexOf($"("));
+      }
+      //   foreach (var item in details)
+      //   {
+      //     Console.WriteLine("item-----");
+
+
+      //     Console.WriteLine(item);
+
+      //   }
       //   Console.WriteLine("============================");
 
       //   var monsterBar = data[0].ChildNodes.Select(s => s.InnerText).ToList();
